5013ccc69c1d6b3a70065b15c58b50f8cde04992
Joseph Post <joe@jcpst.com>
2021-11-25 08:25:51 -0600

    pwa tweaks

--- index-50a289b02d18aab0.js
+++ index-50a289b02d18aab0.js
@@ -259,7 +259,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b3333b51beff0923_bg.wasm', import.meta.url);
+        input = new URL('index-50a289b02d18aab0_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
